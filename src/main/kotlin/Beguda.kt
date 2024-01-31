be25c895b9f5040dbb87534974560a6f57a0bb24
ulisescastell <ulisescastell6@gmail.com>
2024-01-31 12:00:57 +0100

    configure Beguda.kt and Main.kt files

--- src/main/kotlin/Beguda.kt
+++ src/main/kotlin/Beguda.kt
@@ -1,3 +1,15 @@
-class Beguda {
+class Beguda() {
+    private var nom:String = ""
+    private var preu:Double = 0.0
+    private var teSucre: Boolean = false
 
+    constructor(nom: String, preu: Double, teSucre: Boolean):this() {
+        this.nom = nom
+        this.preu = preu
+        this.teSucre = teSucre
+    }
+
+    override fun toString(): String {
+        return "Tipus: ${this.nom}, Preu: ${this.preu}, Ensucarat: ${this.teSucre}}"
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,25 @@
 fun main () {
+
+    println("-----------------------------")
+    println("Bolleria/pasta: ")
     val croissant: Pasta = Pasta("Croissant", 1.75, 45.0, 200)
     val ensaimada: Pasta = Pasta("Ensaimada", 2.25, 120.0, 670)
     val donut: Pasta = Pasta("Donut", 2.0, 75.0, 400)
     println(croissant)
     println(ensaimada)
     println(donut)
+
+    println("-----------------------------")
+    println("Begudes:")
+    println("-----------------------------")
+
+    val aigua = Beguda("Aigua", 1.00, false)
+    val cafeTallat = Beguda("Cafè tallat", 1.35, false)
+    val teVermell = Beguda("Té vermell", 1.50, false)
+    val cola = Beguda("Cola", 1.65, true)
+
+    println(aigua)
+    println(cafeTallat)
+    println(teVermell)
+    println(cola)
 }
